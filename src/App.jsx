401a074013dbaf9c495fc96c3830582d034a6f1a
KhaiMinhVo <khaivpmse184623@fpt.edu.vn>
2025-09-16 17:45:05 +0700

    Family Chart

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
+import News from "./pages/News";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
                   <Route path="/login" element={<Login />} />
                   <Route path="/register" element={<Register />} />
                  <Route path="/forgot-password" element={<ForgotPassword />} />
+                 <Route path="/news" element={<News />} />
         </Routes>
       </BrowserRouter>
     </>
